830af1ae2fd3e52937d3190d6ef5a98281771f3c
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2016-01-10 19:59:58 +0100

    no message

--- myproject/application/view/generic/buscador.php
+++ myproject/application/view/generic/buscador.php
@@ -1,4 +1,6 @@
 <div class="container">
+<a href="<?=URL . 'proyecto/crear'?>">Crear un nuevo proyecto</a>
+<br/><br/>
 <form action="<?= URL . $data['destino']?>" method="post">
 	<input type="search" name="buscar">
 	<input type="submit" value="enviar">
