f7dafb473ad0b89e512cff80482b90a83e1c63c6
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2016-01-10 20:21:35 +0100

    no message

--- myproject/application/view/proyecto/formulario.php
+++ myproject/application/view/proyecto/formulario.php
@@ -1,5 +1,10 @@
 <div class="container">
-	<form action="<?=URL . 'proyecto/crear'?>" method="post">
-
+	<form action="<?=URL . $data['destino']?>" method="post">
+	<input type="text" name="cliente" value="<?php mostrarDatos($data['proyecto'],'cliente')?>">
+	<input type="text" name="promocion" value="<?php mostrarDatos($data['proyecto'],'promocion')?>">
+	<input type="text" name="fecha de inicio" value="<?php mostrarDatos($data['proyecto'],'fecha de inicio')?>">
+	<input type="text" name="fecha de fin" value="<?php mostrarDatos($data['proyecto'],'fecha de fin')?>">
+	<input type="text" name="fecha prevista" value="<?php mostrarDatos($data['proyecto'],'fecha prevista')?>">
+	<input type="text" name="estado" value="<?php mostrarDatos($data['proyecto'],'estado')?>">
 	</form>
 </div>
