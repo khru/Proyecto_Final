343364fe247824dd0193e81ed9fa23567587efa8
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2016-01-10 20:40:32 +0100

    no message

--- myproject/application/view/proyecto/listardeshabilitados.php
+++ myproject/application/view/proyecto/listardeshabilitados.php
@@ -6,7 +6,7 @@
 <table border="1" style="background-color:gray">
 	<tr>
 <?php 	foreach ($claves as $clave => $valor) : ;	//Se listan las cabeceras de la tabla?>
-		
+
 			<th><?=$valor?></th>
 
 <?php 	endforeach ?>
@@ -16,12 +16,12 @@
 <?php   foreach ($data['deshabilitados'] as $clave => $valor) : //Se listan los datos?>
 	<tr>
 		<?php foreach ($valor as $clave => $datos) : ?>
-			
+
 				<td><?=$datos?></td>
-			
+
 		<?php endforeach ?>
-				<td><a href="#">Editar</a></td>
-				<td><a href="<?=URL . 'proyectos/habilitar/' . $valor['id']?>">Habilitar</a></td>
+				<td><a href="<?=URL . 'proyecto/editar'?>">Editar</a></td>
+				<td><a href="<?=URL . 'proyecto/habilitar/' . $valor['id']?>">Habilitar</a></td>
 	</tr>
   <?php endforeach ?>
 </table>
