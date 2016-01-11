c253a76063ee9249b2603fe36cb119393e10dbaf
khru <evrtrabajo@gmail.com>
2016-01-11 14:10:22 +0100

    Mejorando inserción

--- myproject/application/model/personamodel.php
+++ myproject/application/model/personamodel.php
@@ -55,148 +55,154 @@
 		 * @return [type]              [description]
 		 */
 		public static function insert(){
-			if (!$_POST) {
-				// sino hay dolar post devolvemos
-				$errores['generic'][] = "No se han recibido datos";
-				return Validaciones::resultado($errores);
-			} else{
-				// Validamos todas las variables de $_POST
-				$_POST = Validaciones::sanearEntrada($_POST);
-				// Comprobamos que los campos requeridos están sino error
-				if (isset($_POST["nombre"]) && isset($_POST["apellidos"]) && isset($_POST["email"])) {
+			// Comprobamos que los campos requeridos están sino error
+			if (isset($_POST["nombre"]) && isset($_POST["apellidos"]) && isset($_POST["email"])) {
+				// variable del método para poder hacer un rapido bindeo de parametros y errores
+				$errores = [];
+				$campos = [];
+				//  validamos el nombre
+				if (($err = Validaciones::validarNombre($_POST["nombre"])) !== true) {
+					$errores["nombre"] = $err;
+				} else {
+					// si hay nombre lo ponemos en un array
+					$campos[":nombre"] = $_POST["nombre"];
+				}
 
-						// variable del método para poder hacer un rapido bindeo de parametros y errores
-						$errores = [];
-						$campos = [];
-						//  validamos el nombre
-						if (($err = Validaciones::validarNombre($_POST["nombre"])) !== true) {
-							$errores["nombre"] = $err;
-						} else {
-							// si hay nombre lo ponemos en un array
-							$campos[":nombre"] = $_POST["nombre"];
-						}
+				// Validamos los apellidos
+				if (($err = Validaciones::validarApellidos($_POST["apellidos"])) !== true) {
+					$errores["apellidos"] = $err;
+				} else {
+					// si son validos los apellidos lo preparamos para el bindeo
+					$campos[":apellidos"] = $_POST["apellidos"];
+				}
 
-						// Validamos los apellidos
-						if (($err = Validaciones::validarApellidos($_POST["apellidos"])) !== true) {
-							$errores["apellidos"] = $err;
-						} else {
-							// si son validos los apellidos lo preparamos para el bindeo
-							$campos[":apellidos"] = $_POST["apellidos"];
-						}
+				// validamos el email
+				if (($err = Validaciones::validarEmail($_POST["email"])) !== true) {
+					$errores["email"] = $err;
+				} else {
+					// si es valido, preparamos para el bindeo
+					$campos[":email"] = $_POST["email"];
+				}
 
-						// validamos el email
-						if (($err = Validaciones::validarEmail($_POST["email"])) !== true) {
-							$errores["email"] = $err;
-						} else {
-							// si es valido, preparamos para el bindeo
-							$campos[":email"] = $_POST["email"];
-						}
+				if (isset($_POST["direccion"])) {
+					// Si existe la dirección se valida
+					if (($err = Validaciones::validarDireccion($_POST["direccion"])) !== true) {
+						$errores["direccion"] = $err;
+					} else {
+						// si existe, preparamos el bindeo
+						$campos[":direccion"] = $_POST["direccion"];
+					}
+				}
 
-						if (isset($_POST["direccion"])) {
-							// Si existe la dirección se valida
-							if (($err = Validaciones::validarDireccion($_POST["direccion"])) !== true) {
-								$errores["direccion"] = $err;
-							} else {
-								// si existe, preparamos el bindeo
-								$campos[":direccion"] = $_POST["direccion"];
-							}
-						}
-
-						// si existe la provincia
-						if (isset($_POST["provincia"])) {
-							// se valida la provincia
-							if (($err = Validaciones::validarId($_POST["provincia"])) !== true) {
-								$errores["provincia"] = $err;
-							} else {
+				// si existe la provincia
+				if (isset($_POST["provincia"])) {
+					// se valida la provincia
+					if (($err = Validaciones::validarId($_POST["provincia"])) !== true) {
+						$errores["provincia"] = $err;
+					} else {
+						// comprobamos que dicho id exista en la base de datos
+						// Sino lanzamos un error
+						try {
+							$conn = DBPDO::getInstance()->getDatabase();
+							$ssql = "SELECT * FROM categoria WHERE id = :id";
+							$prepare = $conn->prepare($ssql);
+							$prepare->bindParam(":id", $id, PDO::PARAM_INT);
+							$prepare->execute();
+							if ($prepare->rowCount() === 1) {
 								// si existe la preparo
 								$campos[":provincia"] = $_POST["provincia"];
+							} else{
+								$errores["provincia"][] = "La provincia no existe";
 							}
-						}
+						} catch (PDOException $e) {
+							return $errores['generic'][] = "Error en la base de datos";
+						}// fin del try - catch
+					}
+				}// si existe provincia
 
-						// si exite el nif
-						if (isset($_POST["nif"])) {
-							// se valida
-							if (($err = Validaciones::validarNif($_POST["nif"])) !== true) {
-								$errores["nif"] = $err;
+				// si exite el nif
+				if (isset($_POST["nif"])) {
+					// se valida
+					if (($err = Validaciones::validarNif($_POST["nif"])) !== true) {
+						$errores["nif"] = $err;
+					} else {
+						// si existe la preparo
+						$campos[":nif"] = $_POST["nif"];
+					}
+				}
+
+				// si existe telefono
+				if (isset($_POST["telefono"])) {
+					// se valida
+					if (($err = Validaciones::validarTelefono($_POST["telefono"])) !== true) {
+						$errores["telefono"] = $err;
+					} else {
+						// si existe la preparo
+						$campos[":telefono"] = $_POST["telefono"];
+					}
+				}
+				// si exite newsletter
+				if (isset($_POST["newsletter"])) {
+					// se valida
+					if (($err = Validaciones::validarTelefono($_POST["newsletter"])) !== true) {
+						$errores["newsletter"] = $err;
+					} else {
+						// si existe la preparo
+						$campos[":newsletter"] = $_POST["newsletter"];
+					}
+				}
+
+				// si no hay errores
+				if (!$errores) {
+					try{
+						// Montamos las consultas
+						$fields = "";
+						$value = "";
+						// $values = " :campo1 :campo2 ..."
+						// $fields = "campo1, campo2,"
+						foreach ($campos as $indice => $valor) {
+							$values .= " " . $indice;
+							$aux = mb_substr($indice, 1);
+							$fields .= $aux . ",";
+						}
+						// le quito la última coma de más
+						$fields = trim($fields, ",");
+						// conexión a la base de datos
+						$conn = DBPDO::getInstance()->getDatabase();
+						// consulta de la base de datos
+						// Añadimos a la consulta la fecha de alta formateada
+						$fecha = date("Y-m-d");
+						$ssql = "INSERT INTO persona($fields) VALUES ($values, $fecha)";
+						// preparamos la consulta
+
+						$query = $conn->prepare($ssql);
+						// Hacemos el bindeo de parametros
+						foreach ($campos as $indice => $valor) {
+							if ($indice == ":newsletter" || $indice == ":provincia") {
+								$query->bindParam($indice, $valor,PDO::PARAM_INT);
 							} else {
-								// si existe la preparo
-								$campos[":nif"] = $_POST["nif"];
+								$query->bindParam($indice, $valor,PDO::PARAM_STR);
 							}
 						}
-
-						// si existe telefono
-						if (isset($_POST["telefono"])) {
-							// se valida
-							if (($err = Validaciones::validarTelefono($_POST["telefono"])) !== true) {
-								$errores["telefono"] = $err;
-							} else {
-								// si existe la preparo
-								$campos[":telefono"] = $_POST["telefono"];
-							}
+						$query->execute();
+						if ($query->rowCount === 1) {
+							return true;
 						}
-						// si exite newsletter
-						if (isset($_POST["newsletter"])) {
-							// se valida
-							if (($err = Validaciones::validarTelefono($_POST["newsletter"])) !== true) {
-								$errores["newsletter"] = $err;
-							} else {
-								// si existe la preparo
-								$campos[":newsletter"] = $_POST["newsletter"];
-							}
-						}
+						return false;
 
-						// si no hay errores
-						if (!$errores) {
-							try{
-								// Montamos las consultas
-								$fields = "";
-								$value = "";
-								// $values = " :campo1 :campo2 ..."
-								// $fields = "campo1, campo2,"
-								foreach ($campos as $indice => $valor) {
-									$values .= " " . $indice;
-									$aux = mb_substr($indice, 1);
-									$fields .= $aux . ",";
-								}
-								// le quito la última coma de más
-								$fields = trim($fields, ",");
-								// conexión a la base de datos
-								$conn = DBPDO::getInstance()->getDatabase();
-								// consulta de la base de datos
-								// Añadimos a la consulta la fecha de alta formateada
-								$fecha = date("Y-m-d");
-								$ssql = "INSERT INTO persona($fields) VALUES ($values, $fecha)";
-								// preparamos la consulta
-
-								$query = $conn->prepare($ssql);
-								// Hacemos el bindeo de parametros
-								foreach ($campos as $indice => $valor) {
-									if ($indice == ":newsletter" || $indice == ":provincia") {
-										$query->bindParam($indice, $valor,PDO::PARAM_INT);
-									} else {
-										$query->bindParam($indice, $valor,PDO::PARAM_STR);
-									}
-								}
-								$query->execute();
-								if ($query->rowCount === 1) {
-									return true;
-								}
-								return false;
-
-							} catch (PDOException $e){
-								// Lanzamos una excepción a tratar en el controlador
-								throw new Exception('Error con la base de datos');
-							}
-						} else {
-							// devolvemos los errores
-							return Validaciones::resultado($errores);
-						}
-
+					} catch (PDOException $e){
+						// Lanzamos una excepción a tratar en el controlador
+						throw new Exception('Error con la base de datos');
+					}
 				} else {
-					// si los campos requeridos no existen
-					$errores['generic'][] = "Los campos requeridos no han sido introducidos";
+					// devolvemos los errores
 					return Validaciones::resultado($errores);
 				}
+
+			} else {
+				// si los campos requeridos no existen
+				$errores['generic'][] = "Los campos requeridos no han sido introducidos";
+				return Validaciones::resultado($errores);
 			}
 		}// insert()
 	}
--- myproject/application/model/usuariomodel.php
+++ myproject/application/model/usuariomodel.php
@@ -53,28 +53,88 @@
 		public static function insert(){
 			// Primero tenemos que preparar un bloque try catch
 			$errores = [];
-			try {
-				// empezamos la transacción
-				$conn = DBPDO::getInstance()->getDatabase();
-				$conn->beginTransaction();
-				if (($error = PersonaModel::insert()) === true) {
-					$errores['generic'][] = "El usuario se a insertado correctamente";
+			if ($_POST) {
+				// Validamos todas las variables de $_POST
+				$_POST = Validaciones::sanearEntrada($_POST);
+				try {
+					// empezamos la transacción
+					$conn = DBPDO::getInstance()->getDatabase();
+					$conn->beginTransaction();
+					if (($error = PersonaModel::insert()) === true) {
+						// Comprobaos los campos requeridos en la tabla
+						if (isset($_POST["nick"]) && isset($_POST["pass1"]) && isset($_POST["pass2"]) && isset($_POST["categoria"]) && isset($_POST["carpeta"]) && isset($_POST["img"])) {
+							// Si cualquiera de los campos requeridos
+							// Diese un error deberemos lanzar un rollback
 
-				} elseif (($error = PersonaModel::insert()) === false) {
-					$errores['generic'][] = "El usuario no se a insertado correctamente";
-					// Si no se puede insertar el usuario hacemos un rollback
-					$conn->rollback();
-				} else {
-					// Almacenamos los errores los errores
-					$errores[] = $error;
-				}
+							// validar Nick
+							if (($err = Validaciones::validarNick($_POST["nick"])) !== true) {
+								$errores["nick"] = $err;
+							}
 
+							// Validar contraseña
+							if (($err = Validaciones::validarPassAlta($_POST["pass1"], $_POST["pass2"])) !== true) {
+								$errores["pass"] = $err;
+							}
+
+							// Validar la categoría
+							// En el formulario es un campo select
+							// el cual tiene como value el valor de la BD
+							// de forma que aqui solo hay que validar el id de la categria
+							if (($err = Validaciones::validarPassId($_POST["categoria"])) !== true) {
+								$errores["categoria"] = $err;
+							} else {
+								// comprobamos que dicho id exista en la base de datos
+								// Sino lanzamos un error
+								try {
+									$conn = DBPDO::getInstance()->getDatabase();
+									$ssql = "SELECT * FROM categoria WHERE id = :id";
+									$prepare = $conn->prepare($ssql);
+									$prepare->bindParam(":id", $id, PDO::PARAM_INT);
+									$prepare->execute();
+									if ($prepare->rowCount() === 1) {
+										// si existe la preparo
+										$campos[":provincia"] = $_POST["provincia"];
+									} else{
+										$errores["provincia"][] = "La provincia no existe";
+									}
+								} catch (PDOException $e) {
+									return $errores['generic'][] = "Error en la base de datos";
+								}
+							}// fin de las comprobaciones de categoria
+
+							if ($errores) {
+								$conn->rollback();
+								Validaciones::resultado($errores);
+							}
+
+						} else {
+							// puesto que no tratamos de insertar
+							// solamente personas sino que tenemos que insertar usuarios
+							// que puedan logearse, si los campos del usuario no existen
+							// directamente se hace un rollback
+							$conn->rollback();
+						}
+					} elseif (($error = PersonaModel::insert()) === false) {
+						$errores['generic'][] = "El usuario no se a insertado correctamente";
+						// Si no se puede insertar el usuario hacemos un rollback
+						$conn->rollback();
+					} else {
+						// Almacenamos los errores los errores
+						$errores[] = $error;
+						$conn->rollback();
+						return Validaciones::resultado($errores);
+					}
 
 
 				// terminamos la transacción
-			} catch (PDOException $e) {
+				} catch (PDOException $e) {
 
+				}
+			} else {
+				$errores['generic'][] = "No se han recivido datos";
+				return Validaciones::resultado($errores);
 			}
+
 		}
 	}
 ?>
