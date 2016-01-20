0e8295679436f4274d9f0d1ccc72db415821e7fd
Eduardo Lopez <edu230895@gmail.com>
2016-01-20 18:40:14 +0100

    clientemodel.php actualizado

--- myproject/application/model/clientemodel.php
+++ myproject/application/model/clientemodel.php
@@ -8,7 +8,7 @@
 			 provincia.nombre as 'provincia', fecha_alta as 'fecha de alta', newsletter
 			FROM cliente inner join persona on cliente.id = persona.id
 			 inner join provincia on persona.provincia = provincia.id
-			where persona.habilitado = 1";
+			where persona.habilitado = 1 order by cliente.id";
 			$query = $conn->prepare($ssql);
 			$query->execute();
 			return $query->fetchAll();
@@ -19,9 +19,9 @@
 			$ssql = "SELECT cliente.id, persona.nombre, apellidos,
 			 nombre_corporativo as 'nombre corporativo', email, telefono, nif, direccion,
 			 provincia.nombre as 'provincia', fecha_alta as 'fecha de alta', newsletter
-			FROM cliente inner join persona on cliente.id = persona.id
+			FROM cliente inner join persona on (cliente.id = persona.id)
 			 inner join provincia on persona.provincia = provincia.id
-			where persona.habilitado = 0";
+			where persona.habilitado = 0 order by cliente.id";
 			$query = $conn->prepare($ssql);
 			$query->execute();
 			return $query->fetchAll();
@@ -32,7 +32,7 @@
 			$ssql = "SELECT cliente.id, persona.nombre, apellidos,
 			 nombre_corporativo as 'nombre corporativo', email, telefono, nif, direccion,
 			 provincia.nombre as 'provincia', fecha_alta as 'fecha de alta', newsletter
-			FROM cliente inner join persona on cliente.id = persona.id
+			FROM cliente inner join persona on (cliente.id = persona.id)
 			 inner join provincia on persona.provincia = provincia.id
 			where persona.id = :id";
 			$query = $conn->prepare($ssql);
@@ -83,6 +83,29 @@
 			$query->execute();
 			return $query->fetch();
 		} // getByName()
+
+		public static function getSearch($busqueda){
+			$conn = Database::getInstance()->getDatabase();
+
+			$busqueda = '%' . $busqueda . '%';
+			
+			$ssql = "SELECT cliente.id, persona.nombre, apellidos, nombre_corporativo
+            , email, telefono, nif, direccion, provincia.nombre
+             as 'provincia', fecha_alta, newsletter,habilitado
+            FROM cliente inner join persona on (cliente.id = persona.id)
+             inner join provincia on (persona.provincia = provincia.id)
+            WHERE (cliente.nombre_corporativo like :busqueda OR persona.nombre
+             like :busqueda OR apellidos like :busqueda OR telefono like :busqueda
+             OR nif like :busqueda OR direccion like :busqueda OR fecha_alta like
+             :busqueda OR persona.email like :busqueda OR provincia like :busqueda)
+             order by id";
+
+			$query = $conn->prepare($ssql);
+
+			$query->bindParam(':busqueda', $busqueda, PDO::PARAM_STR);
+			$query->execute();
+			return $query->fetchAll();
+		} // getSearch()
 
 	} // ClienteModel
 ?>
