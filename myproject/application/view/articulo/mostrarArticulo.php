c7d6fc870f5c6e7e07f109f86f1434b426b429d2
luiscavero92 <luiscavero92@gmail.com>
2016-01-21 14:58:21 +0100

    Importante correcion en articulos

--- myproject/application/view/articulo/mostrarArticulo.php
+++ myproject/application/view/articulo/mostrarArticulo.php
@@ -1,3 +1,5 @@
-<h1><?= $data['articulo']['titulo']?></h1>
-<p><?= $data['articulo']['cuerpo']?></p>
+<div class="first">
+<h1><?= $data['articulo']['titulo']?></h1><br>
+<p><?= $data['articulo']['cuerpo']?></p><br>
 <p><i>Este artículo se ha publicado con fecha de: <?= $data['articulo']['fecha_publicacion']?></i></p>
+</div>
