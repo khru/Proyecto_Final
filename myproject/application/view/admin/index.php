540aee032e5c5e1615935eefe941a90bb2f6b31e
luiscavero92 <luiscavero92@gmail.com>
2016-01-21 14:48:11 +0100

    no message

--- myproject/application/view/admin/index.php
+++ myproject/application/view/admin/index.php
@@ -1,15 +1,15 @@
 <div class="container">
 
  <h2>ÚLTIMAS NOTICIAS</h2>
- <p><a href="#">DevWeb se posiciona entre los preferidos para el desarrollo web</a></p>
- <p><a href="#">DevWeb abre un nuevo establecimiento</a></p>
- <p><a href="#">Nuevas Promociones disponibles en DevWeb</a></p>
- <p><a href="#">DevWeb ahora en todas las red sociales</a></p>
- <p><a href="#">Nueva Interfaz en DevWeb</a></p>
+ <p><a href="#">DevWeb se posiciona entre los preferidos para el desarrollo web</a></p><br>
+ <p><a href="#">DevWeb abre un nuevo establecimiento</a></p><br>
+ <p><a href="#">Nuevas Promociones disponibles en DevWeb</a></p><br>
+ <p><a href="#">DevWeb ahora en todas las red sociales</a></p><br>
+ <p><a href="#">Nueva Interfaz en DevWeb</a></p><br>
  <h2>ÚLTIMOS PROYECTOS</h2>
- <p><a href="<?= URL . 'proyecto/mostrarProyecto/1' ?>">Perros S.L</a></p>
- <p><a href="<?= URL . 'proyecto/mostrarProyecto/9' ?>">Funeraria Muerto S.L</a></p>
- <p><a href="<?= URL . 'proyecto/mostrarProyecto/6' ?>">Autoescuela Volante</a></p>
+ <p><a href="<?= URL . 'proyecto/mostrarProyecto/1' ?>">Perros S.L</a></p><br>
+ <p><a href="<?= URL . 'proyecto/mostrarProyecto/9' ?>">Funeraria Muerto S.L</a></p><br>
+ <p><a href="<?= URL . 'proyecto/mostrarProyecto/6' ?>">Autoescuela Volante</a></p><br>
 
  
 </div>
